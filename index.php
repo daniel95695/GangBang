6101cfc5f70ee296c135e8196a9ca5ec5ba2c8be
daniel95695 <93367216+daniel95695@users.noreply.github.com>
2021-11-19 13:47:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,90 @@
 <!DOCTYPE html>
 <html>
-<body>
 
-<h2>HTML Image GangBang</h2>
-<img src="img_girl.jpg" alt="GangBang" width="500" height="600">
+<head>
+    <title>Daniel portfolio</title>
+</head>
 
-</body>
+<style>
+    h1 {
+        font-family: verdana;
+    }
+    
+    h3 {
+        color: red;
+        font-family: serif;
+    }
+    
+    p {
+        font-family: Helvetica;
+    }
+    
+    .button {
+        border: none;
+        color: white;
+        padding: 16px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        transition-duration: 0.4s;
+        cursor: pointer;
+    }
+    
+    .button1 {
+        background-color: white;
+        color: black;
+        border: 2px solid black;
+    }
+    /* Github */
+    
+    .button1:hover {
+        background-color: black;
+        color: white;
+    }
+    
+    .button2 {
+        background-color: white;
+        color: black;
+        border: 2px solid deeppink;
+    }
+    /* Instagram */
+    
+    .button2:hover {
+        background-color: purple;
+        color: black;
+    }
+    .button3 {
+        background-color: black;
+        color: white;
+        border: 2px solid dodgerblue;
+    }
+    /* Telegram */
+    
+    .button3:hover {
+        background-color: purple;
+        color: black;
+    }
+    
+</style>
+<center>
+
+    <body style="background-color:beige;">
+        <h1>Turgunaliev Daniel <br>Web junior Developer and designer</h1>
+        <h3>Can change your vision about web developing!</h3>
+        <p> Since beginning my journey nearly 6 years ago. I have developed amazing web applications.</p>
+        <img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/shrek-forever-after-1587549453.jpg?crop=0.676xw:0.901xh;0.0969xw,0&resize=480:*' alt='Almaz' style='width:200px;height:200px;  margin-left: 900px;'> 
+		<form action='https://github.com/zamlabek' target='_blank'>
+        <button class="button button1">Github</button>
+        </form>
+        <form action='https://instagram.com/abdr.gg?utm_medium=copy_link' target='_blank'>
+        <button class="button button2">Instagram</button>
+        </form>
+        <form action='https://t.me/Daniel' target='_blank'>
+        <button class='button button3'>Telegram</button>
+        </form>
+        <h4>My address:Boston town.<br>My phone number:+996707437743</h4>
+    </body>
+</center>
 </html>
-
